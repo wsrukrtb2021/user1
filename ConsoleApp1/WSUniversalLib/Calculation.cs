f8082ad0de3d0dd70748e08735f830568ba1566b
unknown <lorp.lop@yandex.ru>
2021-06-02 09:25:06 +0300

    123

---
+++ ConsoleApp1/WSUniversalLib/Calculation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WSUniversalLib
+{
+    class Calculation
+    {
+        public int[] productType;
+        public int[] materialType;
+
+        public void GetQuantityForProduct()
+        {
+            
+        }
+    }
+}
