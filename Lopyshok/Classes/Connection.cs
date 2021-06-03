8e6b7560687a39ea5ef560eb784a03021b39badb
unknown <lorp.lop@yandex.ru>
2021-06-03 14:13:50 +0300

    dll

--- Lopyshok/Classes/Connection.cs
+++
@@ -1,9 +0,0 @@
-﻿
-
-namespace Lopyshok.Classes
-{
-    class Connection
-    {
-        public static string String { get; } = "Data Source = WSR; Initial Catalog = user1; user id = user1; password = passuser1";
-    }
-}
